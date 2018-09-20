8cea08c7fc9a932685e525d3219fe2d1a1b9093f
Roman Bulgakov <dik.deroy@gmail.com>
2018-09-20 23:20:26 +0300

    BDZ-17 ru translation and build commands added to docs/build.sh

--- docs/build.sh
+++ docs/build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-export FB_APP_ID=1 TYK_DASHBOARD_TOKEN=1 TYK_GATEWAY_TOKEN=1
 
 # generate code reference
 sphinx-apidoc --ext-autodoc --ext-todo --ext-coverage --ext-viewcode -M -e -o ./apidocs ../src/
+# generate gettext
+make gettext
+# generate po files updates
+sphinx-intl update -p _build/gettext -l ru
+# build lang specific docs
+# make -e SPHINXOPTS="-D language='ru'" html
+
 # build sphinx doc in html
 make html
